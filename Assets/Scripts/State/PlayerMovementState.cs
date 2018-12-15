1521ca12441b3a157be1955598aefa8a945849ed
jiexdrop <jorge.noogueira@gmail.com>
2018-12-15 13:24:01 +0100

    Player movement state

---
+++ Assets/Scripts/State/PlayerMovementState.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementState : State
+{
+    Vector2 move;
+
+    public PlayerMovementState(Character character) : base(character)
+    {
+        move = new Vector2();
+    }
+
+    public override void EndState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void InterpretInput(BaseInput.TypeAction typeAct, BaseInput.Actions acts, Vector2 val)
+    {
+        if(typeAct.Equals(BaseInput.TypeAction.Pressed) && acts.Equals(BaseInput.Actions.RightMovement))
+        {
+            Debug.Log(move);
+            move.Set(1,0);
+        }
+
+        if (typeAct.Equals(BaseInput.TypeAction.Pressed) && acts.Equals(BaseInput.Actions.LeftMovement))
+        {
+            Debug.Log(move);
+            move.Set(-1, 0);
+        }
+
+        if (typeAct.Equals(BaseInput.TypeAction.Pressed) && acts.Equals(BaseInput.Actions.UpMovement))
+        {
+            Debug.Log(move);
+            move.Set(0, 1);
+        }
+
+        if (typeAct.Equals(BaseInput.TypeAction.Pressed) && acts.Equals(BaseInput.Actions.DownMovement))
+        {
+            Debug.Log(move);
+            move.Set(0, -1);
+        }
+     
+        //move.Normalize();
+        character.Move(move);
+
+    }
+
+    public override void NextState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void StartState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UpdateState()
+    {
+        throw new System.NotImplementedException();
+    }
+}
