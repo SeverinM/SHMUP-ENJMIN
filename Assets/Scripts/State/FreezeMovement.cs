9997c469772c88cf043751eb2535ef220c6353f9
jiexdrop <jorge.noogueira@gmail.com>
2019-01-01 15:51:26 +0100

    Début Freeze Movement

---
+++ Assets/Scripts/State/FreezeMovement.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreezeMovement : State
+{
+
+    private float duration;
+
+    private float lastTime;
+
+    public FreezeMovement(Character character, float duration) : base(character)
+    {
+        this.duration = duration;
+    }
+
+    public override void EndState()
+    {
+        Constants.TimeScalePlayer = 1;
+        character.level.Remove(character.gameObject);
+    }
+
+    public override void InterpretInput(BaseInput.TypeAction typeAct, BaseInput.Actions acts, Vector2 val)
+    {
+        base.InterpretInput(typeAct, acts, val);
+    }
+
+    public override void NextState()
+    {
+        
+    }
+
+    public override void StartState()
+    {
+        character.Life -= 1;
+
+        if (character.Life > 0)
+        {
+            character.StartRecovery(duration * 10);
+        }
+
+        character.GetComponent<Collider>().enabled = false;
+        character.GetComponent<MeshRenderer>().enabled = false;
+        Constants.TimeScalePlayer = 0;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+
+    public override void UpdateState()
+    {
+        lastTime += Time.deltaTime;
+
+        if (lastTime > duration)
+        {
+            EndState();
+        }
+
+    }
+}
+
