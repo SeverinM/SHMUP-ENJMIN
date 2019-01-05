e0a2f8bcd57a580b4090d1fbe6a7426afe9fdc31
Vincent <vincent.arnol@orange.fr>
2019-01-05 20:28:20 +0100

    Ajout d'un menu principal
    Création d'un menu principal

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene("Enemies");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
