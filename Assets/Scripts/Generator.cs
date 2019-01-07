367ac39356c5e61d375ab4af70383e27434eb8aa
SeverinM <severinmichaut@gmail.com>
2019-01-07 16:53:38 +0100

    Debut de verrous

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -3,6 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
+public class LockWaveElement
+{
+    public Generator generator;
+    public int number;
+
+    public override bool Equals(object obj)
+    {
+        LockWaveElement elt = (LockWaveElement)obj;
+        if (elt == null)
+        {
+            return false;
+        }
+        else
+        {
+            return (generator == elt.generator && number == elt.number);
+        }
+    }
+
+    public override int GetHashCode()
+    {
+        return generator.GetHashCode() * number.GetHashCode();
+    }
+}
+
 /// <summary>
 /// The goal of this class is to keep the data of the Generator
 /// </summary>
@@ -12,9 +36,14 @@ public class Generator : Character {
     [HideInInspector]
     public List<Wave> allWaves = new List<Wave>();
     public int Count = 0;
+    public int WaveCount = 0;
+    public Dictionary<int, List<LockWaveElement>> AllLocks = new Dictionary<int, List<LockWaveElement>>();
 
     public delegate void noParam();
     public event noParam EveryoneDied;
+
+    public delegate void paramClean(int nb, Generator who);
+    public event paramClean WaveCleaned;
 
     //Combien d'ennemie reste t'il 
     public int EnnemiesLeftToSpawn
@@ -47,6 +76,19 @@ public class Generator : Character {
         if (EveryoneDied != null)
         {
             EveryoneDied();
+        }
+    }
+
+    public void RaiseWaveFinished()
+    {
+        WaveCleaned(WaveCount, this);
+    }
+
+    public void RemoveLock(LockWaveElement elt)
+    {
+        foreach(List<LockWaveElement> eltList in AllLocks.Values)
+        {
+            eltList.RemoveAll(x => x == elt);
         }
     }
 }
