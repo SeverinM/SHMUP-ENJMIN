cabfff218c9c3b4827995c01300b50e7bb3af910
SeverinM <severinmichaut@gmail.com>
2019-01-10 09:56:40 +0100

    Plus de physique quand le bob chase

--- Assets/Scripts/State/MovementState/EnemyMovement.cs
+++ Assets/Scripts/State/MovementState/EnemyMovement.cs
@@ -52,26 +52,20 @@ public class EnemyMovement : State
         deltaPosition = target.transform.position - character.transform.position;
         deltaPosition = new Vector3(deltaPosition.x, 0, deltaPosition.z);
 
-        character.Separate();
+        //character.Separate();
+        character.transform.LookAt(target.transform);
 
-        if (!followLeader)
+        // L'ennemie est proche du joueur
+        if (target.GetComponent<Player>() != null && Vector3.Distance(target.transform.position, character.transform.position) <= enemy.AttackRange)
         {
-            character.transform.LookAt(target.transform);
-            // L'ennemie est proche du joueur
-            if (Vector3.Distance(target.transform.position, character.transform.position) <= enemy.AttackRange)
-            {
-                character.SetState(new EnemyAttack(character, allElems, target.transform));
-            }
+            character.SetState(new EnemyAttack(character, allElems, target.transform));
         }
+
         //La vitesse du personnage est de plus en plus lente au fur et a mesure qu'il s'approche de son leader pour eviter de lui rentrer dedans
-        else
-        {
-            if (target.GetComponent<Player>() == null)
-                character.PersonalScale = Mathf.Clamp(Vector3.Distance(target.transform.position, character.transform.position) / enemy.MoveSpeed, 0, 1);
+        if (target.GetComponent<Player>() == null)
+            character.PersonalScale = Mathf.Clamp(Vector3.Distance(target.transform.position, character.transform.position) / enemy.MoveSpeed, 0, 1);
 
-            character.transform.forward = deltaPosition;
-        }
-
+        character.transform.forward = deltaPosition;
         character.Move(deltaPosition.normalized);
     }
 
